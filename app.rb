cdb9eaadbb76a580f1b70bef89091de69d2aec12
Veronica Lopez <nicaa0695@gmail.com>
2019-08-27 21:21:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,5 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do 
-    erb :user_input
-  end 
   
-  post '/piglatinizer' do 
-    @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
-    
-    erb :results
-  end 
 end
